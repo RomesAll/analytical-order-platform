9bd2b0f52b38fad6b905b3962122b83623134090
RomesAll <romesky845@gmail.com>
2026-07-02 15:54:30 +0300

    feat(domain): добавил репозиторий для операций с продуктами Product и получения, сохранения событий EventRepository

--- app/domain/repositories.py
+++ app/domain/repositories.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
+from datetime import datetime, date
 from uuid import UUID
+from .events import OrderEvent
 from .order import Order
+from .product import Product
 
 class OrderRepository(ABC):
     @abstractmethod
@@ -8,5 +11,35 @@ class OrderRepository(ABC):
         pass
 
     @abstractmethod
-    def save(self, order: Order):
+    def save(self, order: Order) -> Order:
+        pass
+
+class ProductRepository(ABC):
+    @abstractmethod
+    def get_by_id(self, product_id: UUID) -> Product:
+        pass
+
+    @abstractmethod
+    def save(self, product: Product) -> Product:
+        pass
+
+    @abstractmethod
+    def increment_count_product(self, product_id: UUID, amount: int) -> tuple[int, int]:
+        pass
+
+    @abstractmethod
+    def decrement_count_product(self, product_id: UUID, amount: int) -> tuple[int, int]:
+        pass
+
+class EventRepository(ABC):
+    @abstractmethod
+    def save(self, event: OrderEvent):
+        pass
+
+    @abstractmethod
+    def get_order_history(self, order_id: UUID):
+        pass
+
+    @abstractmethod
+    def get_analytics(self, date_analyze: date):
         pass
