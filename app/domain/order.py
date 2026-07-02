dd9256afd57d85c98cbc5841c32db8fcc8bb9aa3
RomesAll <romesky845@gmail.com>
2026-07-02 15:48:50 +0300

    refactor(domain): обновил логику доменной модели хранения заказа Order
    - Выделил корневой агрегат Order, в котором содержится множество дочерних агрегатов
    
    - Выделил дочерний агрегат OrderItem для хранения позиций заказа

--- app/domain/order.py
+++ app/domain/order.py
@@ -1,20 +1,54 @@
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from uuid import UUID
-from typing import TYPE_CHECKING
+from enum import Enum
 
-if TYPE_CHECKING:
-    from app.domain.order_item import OrderItem
+class Status(Enum):
+    CREATED = "created"
+    CONFIRMED = "confirmed"
+    PAYMENT = "payment"
+    SHIPPED = "shipped"
+    DELIVERED = "delivered"
+    CANCELLED = "cancelled"
+
+@dataclass
+class OrderItem:
+    product_id: UUID
+    product_name: str
+    quantity: int
+    price_unit: int
+
+    @property
+    def price(self):
+        return self.price_unit * self.quantity
+
+    def dict(self):
+        return {
+            'product_id': self.product_id,
+            'product_name': self.product_name,
+            'quantity': self.quantity,
+            'price_unit': self.price_unit,
+            'price': self.price,
+        }
 
 @dataclass
 class Order:
     id: UUID
     user_id: UUID
-    items: list[OrderItem]
-    status: str = 'на рассмотрении'
-    price: int = 0
+    order_items: list['OrderItem']
+    status: Status = Status.CREATED
     created_at: datetime = datetime.now(tz=timezone.utc)
 
-    def __post_init__(self):
-        if self.price < 0:
-            raise ValueError('Order price cannot be negative')
+    @property
+    def order_price(self) -> int:
+        return sum([item.price for item in self.order_items])
+
+    def dict(self) -> dict:
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'order_items': [item.dict() for item in self.order_items],
+            'order_price': self.order_price,
+            'status': self.status.value,
+            'created_at': self.created_at,
+        }
